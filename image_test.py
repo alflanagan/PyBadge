07928024d74fa30add2b8dc344e074aeb51dbb65
Morgan Stuart <morgansstuart243@gmail.com>
2016-04-11 19:25:21 -0400

    image_test module scales rgb values; Added images/ for testing;

--- image_test.py
+++ image_test.py
@@ -9,15 +9,16 @@ def draw_image_to_badge(image_path,
     if badge is None:
         badge = Badge()
 
-    x_offs = 5
-    y_offs = 5
+    x_offs = 0
+    y_offs = 0
 
     badge.set_background_color(bg_color)
     im = Image.open(image_path)
     pix = im.load()
     for x in range(0, im.size[0]):
         for y in range(0, im.size[1]):
-            pix_val = pix[x,y]
+            # Div by 8 to do rough scaling of 255-0 down to 31-0
+            pix_val =  [int(c/8) for c in pix[x,y]]
             if verbose:
                 print("(%d, %d) = %s" %(x, y, pix_val))
             color = Badge.pack_rgb(*pix_val)
@@ -27,6 +28,6 @@ def draw_image_to_badge(image_path,
 
     badge.swap_buffer()
 
-#p = "tests/smiley.jpg"
-p = "tests/bob.jpg"
+#p = "images/smiley.jpg"
+p = "images/kroo.jpg"
 draw_image_to_badge(p, bg_color='red')
