ed14d7d96b0a7069562de322ae406d6a6e292b94
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2016-08-03 20:17:29 -0400

    Added clearmem and setmem

--- forth/ex_23_1.fs
+++ forth/ex_23_1.fs
@@ -1,3 +1,6 @@
+\ routines for allocating memory and equivalents of calloc() and memset()
+\ see 
+
 : vsum ( addr u -- n )
     \ n is the sum of u cells, with the first cell at addr
     0 -rot 0  u+do  \ stack in loop:  sum addr
@@ -7,3 +10,13 @@
         \ addr value sum
         + swap
     loop drop ;
+
+: clearmem ( addr u -- )
+    \ set cells from addr to addr+u to 0
+    0 u+do
+        dup 0 swap i cells + ! loop ;
+
+: setmem ( addr u n -- )
+    \ set cells from addr to addr+u to n
+    swap 0 u+do
+        2dup swap i cells + ! loop ;
--- forth/test_ex_23_1.fs
+++ forth/test_ex_23_1.fs
@@ -14,4 +14,11 @@ create v1
 
 ." sum is = "
 
-v1 1 vsum . CR bye
+v1 1 vsum . CR
+
+variable v20 20 cells allot
+
+v20 20 vsum . CR
+
+: testxx ( -- ) 20 0 u+do
+    v20 i cells + @ . CR loop ;
