4c3150f5268ca3cbb2dbc1416f724eb709e59134
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2016-09-30 15:35:48 -0400

    optimizations, and fix fcls

--- forth/fuseful.fs
+++ forth/fuseful.fs
@@ -3,7 +3,8 @@
 \ various useful routines for floating-point values
 \ mostly implementing standard integer ops for fp stack
 
-: f-rot frot frot ;
+: f-rot ( f1 f2 f3 -- f3 f1 fs )
+    frot frot ;
 
 : f2swap ( f1 f2 f3 f4 -- f3 f4 f1 f2 )
     pad f! f-rot pad f@ f-rot ;
@@ -11,14 +12,16 @@
 : f2dup ( f1 f2 -- f1 f2 f1 f2 )
     fover fover ;
 
-: fcls ( f1 f2 ... -- )
+: fcls ( f1 .. fn -- )
     \ clear float stack
     fdepth 0 u+do
-        drop
+        fdrop
     loop ;
 
-: f2over ( f1 f2 f3 f4 -- f1 f2 f3 f4 f1 f2 )
-    pad f! pad float + f! f2dup pad float + f@ f-rot pad f@ f-rot ;
+: f2over ( f1 f2 -- f1 f2 f1 f2 )
+    fover fover ;
 
-: f3dup ( u1 u2 u3 -- u1 u2 u3 u1 u2 u3 )
-    fdup f2over frot ;
+: f3dup ( f1 f2 f3 -- f1 f2 f3 f1 f2 f3 )
+    \ fdup f2over frot ;
+    2 fpick 2 fpick 2 fpick ; \ probably faster
+
