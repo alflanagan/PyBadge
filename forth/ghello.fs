eb47e729e5841de6e5ab46d34ce3167b396213ee
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2016-10-29 08:53:26 -0400

    upgrade to WingIDE 6

---
+++ forth/ghello.fs
@@ -0,0 +1,17 @@
+: dumps
+    10
+    begin
+        depth 1 = if
+            drop
+            exit
+        then
+        swap
+        .
+        8 +
+        dup
+        .
+    again
+;
+
+5 4 3 8 2 1 5 dumps
+bye
