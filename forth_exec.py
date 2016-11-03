1983b640b47d724fab121d8cec9d545ab1de381c
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2016-11-03 09:00:03 -0400

    On write failure, attempt to reconnect

--- forth_exec.py
+++ forth_exec.py
@@ -4,8 +4,7 @@
 from pathlib import Path
 import time
 
-from tkinter import constants
-from tkinter.ttk import Frame, Button, Style, Label
+from tkinter.ttk import Frame, Button, Label
 from tkinter import tix
 
 from BadgeSerial import BadgeSerialException
@@ -17,16 +16,14 @@ class Application(Frame):
     """The main Tk application, a simple dialog."""
     def __init__(self, master=None):
         super().__init__(master)
+        self.badge = None
         self.grid()
         self.columnconfigure(0, minsize=200)
         self.columnconfigure(1, minsize=200)
         self.rowconfigure(0, minsize=300)
         self.rowconfigure(3, minsize=30)
-        try:
-            self.badge = Badge()
-        except BadgeSerialException:
-            self.badge = None  # connect later?
         self.create_widgets()
+        self.connect()
 
     def create_widgets(self):
         """Sets up dialog elements."""
@@ -34,7 +31,8 @@ class Application(Frame):
                                         pattern="*.fs", directory="forth")
         # self.select["textVariable"] = self.forth_file
         self.select.grid(row=0, columnspan=2, sticky='n'+'w'+'e')
-        self.connect_btn = Button(self, text="Connect", command=self.connect)
+        self.connect_btn = Button(self, text="Connect",
+                                  command=self.toggle_connect)
         self.connect_btn.grid(row=1, column=0, columnspan=2)
         self.exec_btn = Button(self, text="Execute", command=self.send_file)
         self.exec_btn.state(["disabled"])
@@ -49,25 +47,45 @@ class Application(Frame):
             self.connect_btn.state(["disabled"])
             self.connect_status.config(text="Connected: " + self.badge.os_device)
 
-    def send_file(self):
+    def send_file(self, _retry=False):
         """Send the selected file to the badge."""
-        # oddly, very first set LED seems to not set correct color
-        self.badge.led(0, 0, 128)
-        self.badge.led(0, 0, 128)
-        with open(self.select.cget("value"), 'r') as forthin:
-            self.badge.forth_run(forthin.read())
-        time.sleep(1)  # because forth_run() may be too fast
-        self.badge.led(0, 128, 0)
+        try:
+            # oddly, very first set LED seems to not set correct color
+            self.badge.led(0, 0, 128)
+            self.badge.led(0, 0, 128)
+            with open(self.select.cget("value"), 'r') as forthin:
+                self.badge.forth_run(forthin.read())
+            time.sleep(1)  # because forth_run() may be too fast
+            self.badge.led(0, 128, 0)
+        except IOError:
+            if not _retry:
+                self.connect()
+                self.send_file(True)
+            else:
+                raise
+
+    def toggle_connect(self):
+        "If connected, disconnect, otherwise connect."
+        if self.connect_btn.cget("text") == "Connect":
+            self.connect()
+        else:
+            self.disconnect()
+
+    def disconnect(self):
+        "Disconnect from current badge."
+        isinstance(self.badge, Badge)
+        self.badge.close()
+        self.badge = "None"
+        self.connect_btn.config(text="Connect")
+        self.connect_status.config(text="Not connected.")
 
     def connect(self):
-        """Attempt to connect to a badge; disable Connect button if successful."""
-        # TODO: replace 'Connect' with 'Disconnect'
+        """Attempt to connect to a badge; toggle Connect button if successful."""
         try:
             self.badge = Badge()
-            self.connect_btn.state(["disabled"])
             self.connect_status.config(text="Connected: " + self.badge.os_device)
+            self.connect_btn.config(text="Disconnect")
         except BadgeSerialException:
-            self.connect_btn.state(["!disabled"])
             self.connect_status.config(text="Not connected")
 
     def on_file_selected(self, selected_file):
